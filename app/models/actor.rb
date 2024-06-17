2d955cc2f1a4571561dcabc9db205b14ed0e683d
Chinemelum Iloegbunam <52719448+Cindellai@users.noreply.github.com>
2024-06-17 20:55:46 +0000

    completed

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -11,4 +11,7 @@
 #  updated_at :datetime         not null
 #
 class Actor < ApplicationRecord
+  has_many :characters  # Defined earlier
+
+    # ... other methods of the Actor class
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -10,4 +10,7 @@
 #  movie_id   :integer
 #
 class Character < ApplicationRecord
+  belongs_to :actor  # Character belongs to an actor
+  belongs_to :movie  # Character belongs to a movie
+  # ... other methods of the Character class
 end
